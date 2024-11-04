cf8830e381c0029d1d6f58ede5e8e2ab9a2f2861
karmesh-behera <140515712+karmesh-behera@users.noreply.github.com>
2024-11-04 18:49:46 -0500

    Add files via upload

---
+++ pascaltriangle.py
@@ -0,0 +1,42 @@
+
+def inefftriangle(n):
+    a = []
+
+    for i in range(n):
+        a.append([])
+        a[i].append(1)
+
+        for j in range(1, i):
+            a[i].append(a[i - 1][j - 1] + a[i - 1][j])
+        if (n != 0):
+            a[i].append(1)
+
+    for i in range(n):
+        print("   " * (n - i), end = " ", sep = " ")
+
+        for j in range(0, i + 1):
+            print('{0:5}'.format(a[i][j]), end = " ", sep = " ")
+        print()
+
+
+def triangle(n):
+    
+
+
+
+
+
+
+def main():
+    n = int(input("Enter number of rows: "))
+    inefftriangle(n)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
